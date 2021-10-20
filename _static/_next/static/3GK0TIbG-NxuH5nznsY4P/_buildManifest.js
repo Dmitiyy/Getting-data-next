91d0013c74d220cdcf9e5ff29fc4249f6986a397
Dmitriy <dmitriyy311@gmail.com>
2021-10-20 19:56:51 +0300

    added new script

---
+++ _static/_next/static/3GK0TIbG-NxuH5nznsY4P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/58-bc23bee4e783ac00450c.js","static/chunks/pages/index-bc7320e3bc0c98ed8204.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
